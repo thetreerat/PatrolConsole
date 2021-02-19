cf14a0a6455d855dc373171699011c35f28c7bc4
Harold Clark <harold.clark@openscg.com>
2021-02-18 20:49:11 -0500

    added second test message to body

--- UDPTestClient.py
+++ UDPTestClient.py
@@ -1,6 +1,7 @@
 # Author: Harold Clark
-# Copyright Harold Clark 2019
+# Copyright Harold Clark 2021
 #
+from DMRmessage import Message
 import socket
 
 class  UDPClient(object):
@@ -35,13 +36,22 @@ Purpose        : This Class is a temlplete file
             self.udpCleintSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def requestData(self, SendData):
-        sentbytesCount = self.udpCleintSocket.sendto(SendData.encode(), self.destination)
+        sentbytesCount = self.udpCleintSocket.sendto(SendData, self.destination)
         recievedBytes = self.udpCleintSocket.recvfrom(self.getbytes)
         print(recievedBytes)
 
 if __name__ == "__main__":
+
     N = UDPClient(IP="192.168.1.17", Port=4007)
     print(N)
+    msg = "DoSomeThBill T -HILL.  Hello to you".encode()
+    msg2 = "SignIN  Unregistered   Hal C -AP".encode()
+    inet = (N.ServerIP, N.ServerPort)
+    msgandAddress = 
+    m = Message(msg,inet)
+    m2 = Message(msg2,inet)
     N.CreateSocket()
-    N.requestData("Test")
+    N.requestData(m.encoded)
+    
+    N.requestData(m2.encoded)
     #N.About()
